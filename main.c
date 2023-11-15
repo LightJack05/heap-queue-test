b62526cd05614031b44ebd4ad58664a44175255a
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2023-11-15 11:56:25 +0100

    Implemented function pointers

--- main.c
+++ main.c
@@ -5,12 +5,8 @@
 int main()
 {
     defineGenericQueue(int);
-    struct Queue(int) *intQueue = (struct Queue(int) *)malloc(sizeof(struct Queue(int)));
-    enQueue(int)(intQueue, 10);
-    printf("%d\n", deQueue(int)(intQueue));
-
-    defineGenericQueue(char);
-    struct Queue(char) *charQueue = (struct Queue(char) *)malloc(sizeof(struct Queue(char)));
-    enQueue(char)(charQueue, 'c');
-    printf("%c\n", deQueue(char)(charQueue));
+    struct Queue(int) *intQueue = newQueue(int);
+    intQueue->enQueue(intQueue, 16);
+    printf("%d\n", intQueue->deQueue(intQueue));
+    printf("%d\n", intQueue->isEmpty(intQueue));
 }
--- queue.c
+++ queue.c
@@ -4,15 +4,15 @@
 /*
 Example usage:
 
-struct Queue *queueOne = (struct Queue *)malloc(sizeof(struct Queue));
-queueOne->start = NULL;
-intQueue_enQueue(queueOne, 5);
-intQueue_enQueue(queueOne, 6);
-intQueue_enQueue(queueOne, 4);
-
-printf("%d\n", intQueue_deQueue(queueOne));
-printf("%d\n", intQueue_deQueue(queueOne));
-printf("%d\n", intQueue_deQueue(queueOne));
+    defineGenericQueue(char);
+    struct Queue(char) *charQueue = newQueue(char);
+    charQueue->enQueue(charQueue, 'A');
+    charQueue->enQueue(charQueue, 'B');
+    charQueue->enQueue(charQueue, 'C');
+    printf("%c\n", charQueue->deQueue(charQueue));
+    printf("%c\n", charQueue->deQueue(charQueue));
+    printf("%c\n", charQueue->deQueue(charQueue));
+    printf("%d\n", charQueue->isEmpty(charQueue));
 */
 
 #define defineGenericQueue(T)                                                                                  \
@@ -21,11 +21,14 @@ printf("%d\n", intQueue_deQueue(queueOne));
         T data;                                                                                                \
         struct QueueElement##T *ptrNextElement; /*Pointer to the next element in the queue, enqueued later*/   \
     };                                                                                                         \
-                                                                                                               \
     struct Queue##T                                                                                            \
     {                                                                                                          \
         struct QueueElement##T *end;   /* Pointer to last element enqueued*/                                   \
         struct QueueElement##T *start; /* Pointer to first element in queue*/                                  \
+        int (*isEmpty)(struct Queue##T *);                                                                     \
+        void (*enQueue)(struct Queue##T *, T);                                                                 \
+        T(*deQueue)                                                                                            \
+        (struct Queue##T *);                                                                                   \
     };                                                                                                         \
                                                                                                                \
     int isQueueEmpty##T(struct Queue##T *queue)                                                                \
@@ -50,7 +53,7 @@ printf("%d\n", intQueue_deQueue(queueOne));
         queue->end = newElement;                                                                               \
     }                                                                                                          \
                                                                                                                \
-    int deQueue##T(struct Queue##T *queue)                                                                     \
+    T deQueue##T(struct Queue##T *queue)                                                                       \
     {                                                                                                          \
         if (isQueueEmpty##T(queue))                                                                            \
         {                                                                                                      \
@@ -68,10 +71,20 @@ printf("%d\n", intQueue_deQueue(queueOne));
         free(queue->start);                                                                                    \
         queue->start = newStart;                                                                               \
         return startValue;                                                                                     \
+    }                                                                                                          \
+                                                                                                               \
+    struct Queue##T *newQueue##T()                                                                             \
+    {                                                                                                          \
+        struct Queue##T *queue = (struct Queue##T *)malloc(sizeof(struct Queue##T));                           \
+        queue->isEmpty = isQueueEmpty##T;                                                                      \
+        queue->enQueue = enQueue##T;                                                                           \
+        queue->deQueue = deQueue##T;                                                                           \
+        return queue;                                                                                          \
     }
 
 #define QueueElement(T) QueueElement##T
 #define Queue(T) Queue##T
-#define isQueueEmpty(T) isQueueEmpty##T
-#define enQueue(T) enQueue##T
-#define deQueue(T) deQueue##T
+// #define isQueueEmpty(T) isQueueEmpty##T
+// #define enQueue(T) enQueue##T
+// #define deQueue(T) deQueue##T
+#define newQueue(T) newQueue##T()
