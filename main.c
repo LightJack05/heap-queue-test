15ac99af7d44285561e53cb8f78f7e6269c17aeb
LightJack05 <66321084+LightJack05@users.noreply.github.com>
2023-11-15 09:07:33 +0100

    Added example usage

--- main.c
+++ main.c
@@ -15,5 +15,10 @@ int main()
     defineGenericQueue(int);
     struct Queue(int) *intQueue = (struct Queue(int) *)malloc(sizeof(struct Queue(int)));
     enQueue(int)(intQueue, 10);
-    printf("%d", deQueue(int)(intQueue));
+    printf("%d\n", deQueue(int)(intQueue));
+
+    defineGenericQueue(char);
+    struct Queue(char) *charQueue = (struct Queue(char) *)malloc(sizeof(struct Queue(char)));
+    enQueue(char)(charQueue, 'c');
+    printf("%c\n", deQueue(char)(charQueue));
 }
